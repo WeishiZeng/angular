dfc07de86f5aae571eeaba6cfe4c366d4b7e9bb6
Weishi Zeng <amwish.zeng@gmail.com>
2018-01-27 22:23:54 -0800

    adding leader service

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -8,8 +8,9 @@ import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'menu', component: MenuComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
   { path: 'contactus', component: ContactComponent },
+  { path: 'aboutus', component: AboutComponent },
   { path: 'dishdetail/:id', component: DishdetailComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { ContactComponent } from './contact/contact.component';
 //service imports
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service'; 
 
 //module imports
 import { AppRoutingModule } from './app-routing/app-routing.module';
@@ -50,7 +51,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule, BrowserModule, FormsModule, HttpModule,
     FlexLayoutModule, AppRoutingModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
