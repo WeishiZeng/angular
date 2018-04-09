1ad941c7d532cc53c214cfa7fe0a5eb7bee5fdc9
Weishi Zeng <wizeng@linkedin.com>
2018-04-08 19:44:24 -0700

    introduce promises

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,7 +20,7 @@ export class DishdetailComponent implements OnInit {
 
     ngOnInit() {
       let id = +this.route.snapshot.params['id'];  //+ sign cast it to numbers
-      this.dish = this.dishservice.getDish(id);
+      this.dishservice.getDish(id).then(dish => this.dish = dish);
     }
 
     goBack(): void {
